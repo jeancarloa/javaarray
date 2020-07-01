e304a2642b7bfdb41a51e6366a9413b6158d10f3
jeancarlo aliendres <jeeanacrlos@gmail.com>
2020-07-01 13:00:20 -0500

    Este es la tarea

---
+++ java.js
@@ -0,0 +1,26 @@
+console.log("HOla mundo");
+var a = 5+8
+var array1 = [8,7,3,"hhh"]
+
+var matriz = [
+    ['la',0,0,0,0,'la'],
+    [0,'lu',0,0,'pa',0],
+    [0,0,'na','la',0,0],
+    [0,0,0,'es',0,0],
+    [0,0,'lo',0,'ro',0],
+    ['ca',0,0,0,0,'ja']]
+
+/*
+array que contenga 5 arrays en forma matricial
+formando una matriz de 5x5, adicional
+los elementos que contiene deben ser todos
+distintos y en la consola se debe llamar
+un elemento de cada array 
+
+opción 1: 1,1.1,"as",[],false
+opción 2: palabra en las diagonales
+*/
+
+console.log(matriz[0][0],matriz[1][1],matriz[2][2],matriz[3][3],matriz[4][4],matriz[5][5])
+console.log(matriz[0][5],matriz[1][4],matriz[2][3],matriz[3][3],matriz[4][2],matriz[5][0])
+
